994c58307087605dc080e0cee8225e950c83f9fd
forma5cv51ramr <forma5cv51ramr@gmail.com>
2023-06-08 18:47:29 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDjFz66a9PqmTwNC7mppbVhGkrCQ_77utk",
+  authDomain: "chatrick3.firebaseapp.com",
+  projectId: "chatrick3",
+  storageBucket: "chatrick3.appspot.com",
+  messagingSenderId: "908974402413",
+  appId: "1:908974402413:web:5954902f8d2b9eb43d8864",
+  measurementId: "G-9NZX6R35WG"
+    });
